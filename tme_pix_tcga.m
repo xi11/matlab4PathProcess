760c03b948b445c58d77e7353d58825fb841b142
xi11 <xiaoxipan3@gmail.com>
2024-01-29 17:06:50 -0600

    initial commitment

---
+++ tme_pix_tcga.m
@@ -0,0 +1,59 @@
+clear
+clc
+close all
+
+
+%src_gp_mask = 'D:\TCGA_BRCA_part\stroma\mask_ss1_update1_DCP20_21max_5_5_final';
+src_gp_mask = '/Volumes/xpan7/project/tcga_tnbc/tmeseg_tcga20x384finetune/mask_ss1_post_tumor21_90000';
+files = dir(fullfile(src_gp_mask, '*.png'));
+
+tableTmp = table("",0,0,0,0,0,'VariableNames',{'ID',...
+    'tumor_pix', 'necrosis_pix', 'stroma_pix', 'fat_pix', 'inflam_pix'});
+k = length(files);
+gp_pix = zeros(k, 5);
+for i = 1:k
+    file_name = files(i).name;
+    wsi_ID = extractBefore(file_name, '_Ss1.png');
+   
+    img = double(imread(fullfile(src_gp_mask, file_name)));
+    temp = [];
+    [m, n, ~] = size(img);
+    mask_digit = zeros(m, n);
+    mask_digit((img(:,:,1)==255 & img(:,:,2)==0 & img(:,:,3)==0)) = 1; %inflam
+    mask_digit((img(:,:,1)==255 & img(:,:,2)==0 & img(:,:,3)==255)) = 2; %necrosis
+    mask_digit((img(:,:,1)==128 & img(:,:,2)==0 & img(:,:,3)==0)) = 3; %tumor
+    mask_digit((img(:,:,1)==255 & img(:,:,2)==255 & img(:,:,3)==0)) = 4; %stroma
+    mask_digit((img(:,:,1)==128 & img(:,:,2)==128 & img(:,:,3)==0)) = 5; %fat
+    
+    
+    for j = 1:5
+        temp(j) = length(find(mask_digit(:)==j));
+    end
+    if max(temp)>0
+    
+    gp_pix(i, 1:5) = temp;  %per
+   
+    end
+    
+    
+    tableTmp.ID(i) = wsi_ID;
+    tableTmp.tumor_pix(i) = gp_pix(i, 3);
+    tableTmp.necrosis_pix(i) = gp_pix(i, 2);
+    tableTmp.stroma_pix(i) = gp_pix(i, 4);
+    tableTmp.fat_pix(i) = gp_pix(i, 5);
+    tableTmp.inflam_pix(i) = gp_pix(i, 1);
+ 
+
+%     tableTmp.tumor_per(i) = gp_pix(i, 3);
+%     tableTmp.necrosis_per(i) = gp_pix(i, 2);
+%     tableTmp.inflam_per(i) = gp_pix(i, 5);
+%     tableTmp.reactive_per(i) = gp_pix(i, 4);
+%     tableTmp.inactive_per(i) = gp_pix(i, 1);
+%     tableTmp.reactive2stroma(i) = gp_pix(i, 6);
+%     tableTmp.reactive2tumor(i) = gp_pix(i, 7);
+%     tableTmp.inactive2tumor(i) = gp_pix(i, 8);
+%           
+end
+writetable(tableTmp, '/Volumes/xpan7/project/tcga_tnbc/tmeseg_tcga20x384finetune/TCGA_BRCA118_tmeTCGAfine384_tumor21_90000_pixel.xlsx')
+
+
