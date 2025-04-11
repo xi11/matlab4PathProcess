4355f91afb4c9604a4f7eb54a4c03fc45ed94561
xi11 <xiaoxipan3@gmail.com>
2025-04-11 16:04:53 -0500

    add tme per

--- tme/tme_per_melanoma.m
+++ tme/tme_per_melanoma.m
@@ -3,7 +3,7 @@ clc;
 close all
 
 
-src_path = '/Volumes/yuan_lab/TIER2/melanoma_vp/pilot1_Mar2025/pgmn_segformer_stainedgeV3_b2b/mask_ss1_x8';
+src_path = '/Volumes/yuan_lab/TIER2/melanoma_vp/pilot1_Mar2025/pgmn_segformer_stainedgeV3/mask_ss1_x8';
 
 dst_path = '/Volumes/yuan_lab/TIER2/melanoma_vp/pilot1_Mar2025/mit-b3-finetuned-TCGAbcssWsss10xLuadMacroMuscle-40x896-20x512-10x256re';
 if ~exist(dst_path, 'dir')
@@ -26,7 +26,7 @@ tissue_colors = {
 
 tissue_labels = {'tumor', 'stroma', 'inflammatory', 'necrosis', ...
     'adipose', 'bronchi', 'microvessel', ...
-    'macrophage', 'alveoli', 'muscle', 'pigment'};
+    'macrophage', 'alveoli', 'muscle', 'pigment', 'tme_pix'};
 result_table = [];
 files = dir(fullfile(src_path, '*.png'));
 for i =1:length(files)
@@ -34,7 +34,7 @@ for i =1:length(files)
     file_name = extractBefore(file_name_raw, '.svs_Ss1.png');
     disp(file_name)
     tme_mask = imread(fullfile(src_path, file_name_raw));
-    region_mask = tme_mask;
+    region_mask = imresize(tme_mask, 0.5, 'nearest');
 
     region_mask_bin = rgb2gray(region_mask) >0;
     if any(region_mask_bin(:))
@@ -48,12 +48,13 @@ for i =1:length(files)
         total_pixels = size(region_colors, 1);
 
         tissue_row = zeros(1, length(tissue_labels));
-        for k = 1:length(tissue_labels)
+        for k = 1:length(tissue_labels)-1 % to remove tme_pix col
             ref_color = tissue_colors{k,1};
             match = all(region_colors == ref_color, 2);
             count = sum(match);
             tissue_row(k) = count / total_pixels * 100;
         end
+        tissue_row(k+1) = total_pixels;
     else
         tissue_row = zeros(1, length(tissue_labels));
     end
@@ -65,7 +66,7 @@ end
 
 T = cell2table(result_table, ...
     'VariableNames', [{'ID'}, tissue_labels]);
-writetable(T, fullfile(dst_path, 'TMEseg_TMEper_pgmnper.xlsx'));
+writetable(T, fullfile(dst_path, 'TMEseg_TMEper_pgmnpix.xlsx'));
 
 
 
