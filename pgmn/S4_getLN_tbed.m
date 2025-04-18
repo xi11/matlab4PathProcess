e4d4f54b91e2d2cf78760384eb2fe9f57fca7271
xi11 <xiaoxipan3@gmail.com>
2025-04-17 21:55:34 -0500

    add LN mask extraction

---
+++ pgmn/S4_getLN_tbed.m
@@ -0,0 +1,34 @@
+clear;
+clc;
+close all
+
+% to get LN annotated by Maria and processed by Xiaoxi for internal data
+% tcga has been confirmed with Maria and annotated by Xiaoxi
+src_path1 = '/Volumes/yuan_lab/TIER2/anthracosis/never_smoker/LN_NS_all_mask/multi';
+src_path2 = '/Volumes/yuan_lab/TIER2/anthracosis/never_smoker_multi/ss1x8overlay_tissue_tbed_remove90000LN_nec';
+dst_path1 = '/Volumes/yuan_lab/TIER2/anthracosis/never_smoker/LN_NS_all_mask/multi_LN_mask';
+
+if ~exist(dst_path1, 'dir')
+    mkdir(dst_path1)
+end
+
+
+
+files = dir(fullfile(src_path1, '*.png'));
+for i =1:length(files)
+    file_name = files(i).name(1: end-11);
+    disp(file_name)
+    mask_LN = imread(fullfile(src_path1, files(i).name));
+    mask_tbed = imread(fullfile(src_path2, [file_name, '.svs_tissue_tbed.png']));
+   
+    [m, n, ~] = size(mask_tbed);
+    mask_LN_bin = (mask_LN(:,:,1) == 255) & (mask_LN(:,:,2) == 255) & (mask_LN(:,:,3) == 255);
+    cc = bwconncomp(mask_LN_bin);
+    stats = regionprops(cc,'Area');
+    idx = find([stats.Area] >= 10000); %dont change
+    BW = ismember(labelmatrix(cc),idx);
+    BW = imresize(BW, [m, n], 'nearest');
+    mask_tbed_ln = 255 *uint8(BW);
+    imwrite(mask_tbed_ln, fullfile(dst_path1, [file_name, '.svs_LN.png']));
+end
+
