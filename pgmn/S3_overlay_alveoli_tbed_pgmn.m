e6fd9531ec3bd1b3d7db22700eac3b31735e02f8
xi11 <xiaoxipan3@gmail.com>
2026-03-17 11:37:34 -0500

    update

--- pgmn/S3_overlay_alveoli_tbed_pgmn.m
+++ pgmn/S3_overlay_alveoli_tbed_pgmn.m
@@ -3,10 +3,10 @@ clc;
 close all
 
 %to overlay tissue, tumor-bed, ss1x8 level
-tme_path = '/Volumes/yuan_lab/TIER2/anthracosis/nlst1225/mit-b3-finetuned-TCGAbcssWsss10xLuadMacroMuscle-40x896-20x512-10x256re/mask_ss1512';
-tbed_path = '/Volumes/yuan_lab/TIER2/anthracosis/nlst1225/tbed1536_ss1/maskLuadLusc_tmeMacro_nonAlveoli_tumor5per_remove10000_smooth30'; % maskLuadLusc_nonTper_nonAlveoli_remove10000_smooth30';
-pgmn_path = '/Volumes/yuan_lab/TIER2/anthracosis/nlst1225/pgmn_segformer_stainedgeV3/mask_ss1_x8';
-dst_path = '/Volumes/yuan_lab/TIER2/anthracosis/nlst1225/ss1x8overlay_alveoli_tbedRefine_remove90000'; %ss1x8overlay_pgmn_alveoli_tbedraw_remove90000';  
+tme_path = '/Volumes/yuan_lab/TIER2/anthracosis/CancerCell2025_lung/he/mit-b3-finetuned-TCGAbcssWsss10xLuadMacroMuscle-40x896-20x512-10x256re/mask_ss1x512';
+tbed_path = '/Volumes/yuan_lab/TIER2/anthracosis/CancerCell2025_lung/he/tbed1536_ss1/maskLuadLusc_pre1178_post_used'; % maskLuadLusc_nonTper_nonAlveoli_remove10000_smooth30';
+pgmn_path = '/Volumes/yuan_lab/TIER2/anthracosis/CancerCell2025_lung/he/pgmn_segformer_stainedgeV3/mask_ss1_x8';
+dst_path = '/Volumes/yuan_lab/TIER2/anthracosis/CancerCell2025_lung/he/ss1x8overlay_pgmn_alveoli_tbedRefine_remove90000'; %ss1x8overlay_pgmn_alveoli_tbedraw_remove90000';  
  
 if ~exist(dst_path, 'dir')
     mkdir(dst_path)
@@ -18,8 +18,8 @@ for i =1:length(files)
     file_name = files(i).name(1: end-13); %-13 / -9
     disp(file_name)
         mask_tbed = imread(fullfile(tbed_path, [file_name, '_tme_tbed.png'])); %_tme_tbed.png / _tbed.png
-        mask_tme = imread(fullfile(tme_path, [file_name, '.svs_Ss1.png']));
-        mask_pgmn = imread(fullfile(pgmn_path, [file_name, '.svs_Ss1.png']));
+        mask_tme = imread(fullfile(tme_path, [file_name, '.tif_Ss1.png'])); %.svs vs .tif
+        mask_pgmn = imread(fullfile(pgmn_path, [file_name, '.tif_Ss1.png']));
         [m, n, ~] = size(mask_pgmn);
         mask_tbed = imresize(mask_tbed, [m, n], 'nearest');
 
@@ -56,10 +56,10 @@ for i =1:length(files)
         mask_final = cat(3,mask_tissue1, mask_tissue2, mask_tissue3);
         
         %%for better visualization pgmn
-        %mask_pgmn_re = mask_pgmn_re(:,:,1);
-        %mask_final(repmat(logical(mask_pgmn_re), [1, 1, 3])) = 255;
-        %mask_final = imresize(mask_final,2,'nearest');
+        mask_pgmn_re = mask_pgmn_re(:,:,1);
+        mask_final(repmat(logical(mask_pgmn_re), [1, 1, 3])) = 255;
+        mask_final = imresize(mask_final,2,'nearest');
 
-        imwrite(mask_final, fullfile(dst_path, [file_name, '.svs_alveoli_tbed.png']))
+        imwrite(mask_final, fullfile(dst_path, [file_name, '.tif_alveoli_tbed.png'])) %.svs or .tif
    
 end
