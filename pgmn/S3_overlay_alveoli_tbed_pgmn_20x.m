1fe52d202f4c23816faa12eee032218ae58ebf3d
xi11 <xiaoxipan3@gmail.com>
2026-03-30 14:08:07 -0500

    update inhouse xenium process

--- pgmn/S3_overlay_alveoli_tbed_pgmn_20x.m
+++ pgmn/S3_overlay_alveoli_tbed_pgmn_20x.m
@@ -6,8 +6,8 @@ close all
 %to overlay tissue, tumor-bed
 tme_path = '/Volumes/yuan_lab/TIER2/anthracosis/xenium_PCF_HE_TMA5primary/he/mit-b3-finetuned-TCGAbcssWsss10xLuadMacroMuscle-40x896-20x512-10x256re/mask_ss1x512';
 tbed_path = '/Volumes/yuan_lab/TIER2/anthracosis/xenium_PCF_HE_TMA5primary/he/tbed1536_ss1/maskLuadLusc_nonTper_nonAlveoli_remove10000_smooth30';
-pgmn_path = '/Volumes/yuan_lab/TIER2/anthracosis/xenium_PCF_HE_TMA5primary/he/pgmn_segformer_stainedgeV3/mask_ss1_x1_filter0fill_dilate23';
-dst_path = '/Volumes/yuan_lab/TIER2/anthracosis/xenium_PCF_HE_TMA5primary/he/fullresoverlay_pgmnClass_dilate23_alveoli_tbedraw_remove160000';  
+pgmn_path = '/Volumes/yuan_lab/TIER2/anthracosis/xenium_PCF_HE_TMA5primary/he/pgmn_segformer_stainedgeV3/mask_ss1_x1';
+dst_path = '/Volumes/yuan_lab/TIER2/anthracosis/xenium_PCF_HE_TMA5primary/he/fullresoverlay_pgmn_alveoli_tbedraw_remove160000'; 
  
 %tme_path = '/Volumes/yuan_lab/TIER2/anthracosis/never_smoker/fig1_demo/mask_10x_tme';
 %tbed_path = '/Volumes/yuan_lab/TIER2/anthracosis/never_smoker/fig1_demo/maskLuadLusc_tmeMacro_tumor5per_remove10000';
@@ -24,7 +24,7 @@ for i =1:length(files)
     disp(file_name)
         mask_tbed = imread(fullfile(tbed_path, [file_name, '_tme_tbed.png'])); %_tme_tbed.png / _tbed.png
         mask_tme = imread(fullfile(tme_path, [file_name, '.tif_Ss1.png']));
-        mask_pgmn = imread(fullfile(pgmn_path, [file_name, '_dilate.png'])); %'.tif_Ss1.png'
+        mask_pgmn = imread(fullfile(pgmn_path, [file_name, '.tif_Ss1.png'])); %, '_dilate.png'
         [m, n, ~] = size(mask_pgmn);
         mask_tbed = imresize(mask_tbed, [m, n], 'nearest');
 
@@ -61,29 +61,29 @@ for i =1:length(files)
         mask_final = cat(3,mask_tissue1, mask_tissue2, mask_tissue3);
         
         %%for better visualization with pgmn as all white
-        %mask_pgmn_re = mask_pgmn_re(:,:,1);
-        %mask_final(repmat(logical(mask_pgmn_re), [1, 1, 3])) = 255;
+        mask_pgmn_re = mask_pgmn_re(:,:,1);
+        mask_final(repmat(logical(mask_pgmn_re), [1, 1, 3])) = 255;
         %%mask_final = imresize(mask_final,2,'nearest');
 
 
-        %%for better visualization with pgmn as in tbed[224, 130, 20] and non-tbed[222,119,174]
-        mask_pgmn_re = mask_pgmn_re(:,:,1);
-        color1 = [224, 130, 20];   % for pixels in both BW3 and mask_pgmn_re
-        color2 = [222,119,174];  % for pixels in mask_pgmn_re but not in BW3
-        
-        % Mask where both BW3 and mask_pgmn_re are true
-        mask_both = BW3 & mask_pgmn_re;
-        
-        % Mask where only mask_pgmn_re is true but not BW3
-        mask_only_pgmn = mask_pgmn_re & ~BW3;
-        
-        % Assign colors to corresponding channels
-        for c = 1:3
-            channel = mask_final(:,:,c);
-            channel(mask_both) = color1(c);
-            channel(mask_only_pgmn) = color2(c);
-            mask_final(:,:,c) = channel;
-        end
+%         %%for better visualization with pgmn as in tbed[224, 130, 20] and non-tbed[222,119,174]
+%         mask_pgmn_re = mask_pgmn_re(:,:,1);
+%         color1 = [224, 130, 20];   % for pixels in both BW3 and mask_pgmn_re
+%         color2 = [222,119,174];  % for pixels in mask_pgmn_re but not in BW3
+%         
+%         % Mask where both BW3 and mask_pgmn_re are true
+%         mask_both = BW3 & mask_pgmn_re;
+%         
+%         % Mask where only mask_pgmn_re is true but not BW3
+%         mask_only_pgmn = mask_pgmn_re & ~BW3;
+%         
+%         % Assign colors to corresponding channels
+%         for c = 1:3
+%             channel = mask_final(:,:,c);
+%             channel(mask_both) = color1(c);
+%             channel(mask_only_pgmn) = color2(c);
+%             mask_final(:,:,c) = channel;
+%         end
        
         imwrite(mask_final, fullfile(dst_path, [file_name, '_alveoli_tbed.png']))
    
